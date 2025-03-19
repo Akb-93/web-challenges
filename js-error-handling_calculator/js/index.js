472927b4430359e85d5845f8d4a19ea8e1a28914
Andreas Buchberger <Buchberger.Andreas@icloud.com>
2025-03-19 15:52:43 +0100

    Solved

--- js-error-handling_calculator/js/index.js
+++ js-error-handling_calculator/js/index.js
@@ -2,15 +2,22 @@ console.clear();
 
 const form = document.querySelector("form");
 const output = form.querySelector("output");
+const errorMessage = document.querySelector(".error");
 
 const operations = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
   multiply: (a, b) => a * b,
   divide: (a, b) => {
-    if (b === 0) {
-      throw new Error("Cannot divide by zero!");
+    try {
+      if (b === 0) {
+        throw new Error("Cannot divide by zero!");
+      }
+    } catch (error) {
+      errorMessage.innerHTML = "you can not divide by 0";
+      console.log("you can not divide by 0");
     }
+
     return a / b;
   },
 };
