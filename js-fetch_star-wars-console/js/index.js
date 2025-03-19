d0f90c77629a8b9b328cfb78f9f551930e020f8b
Andreas Buchberger <Buchberger.Andreas@icloud.com>
2025-03-19 11:53:12 +0100

    solved

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -1,7 +1,25 @@
 console.clear();
 
+// import { renderElement } from "./utils";
+
+// const root = document.body;
+// const jokeSection = JokeSection();
+// root.append(jokeSection);
+
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data.results);
+  const allData = data.results;
+  //   allData.forEach((name) => {
+  //     const dataNames =
+  //   });
+
+  const names = allData.map((charactor) => charactor.name);
+  console.log(names);
+}
 
 fetchData();
+// renderElement();
