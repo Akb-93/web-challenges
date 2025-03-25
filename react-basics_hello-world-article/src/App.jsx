ca428fc38afb57f7b5972e283b590aa469a003a9
Andreas Buchberger <Buchberger.Andreas@icloud.com>
2025-03-25 16:31:07 +0100

    solved

--- react-basics_hello-world-article/src/App.jsx
+++ react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,13 @@
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle></HelloWorldArticle>;
+}
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello</h1>
+      <p>world</p>
+    </article>
+  );
 }
