a8eb2b11a402c362d644beaf47ed4d0e065da317
Andreas Buchberger <Buchberger.Andreas@icloud.com>
2025-03-25 16:53:56 +0100

    solved

--- react-basics_attributes/src/App.jsx
+++ react-basics_attributes/src/App.jsx
@@ -1,5 +1,30 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return <CreateArticle></CreateArticle>;
 }
+
+function CreateArticle() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Hello World!</h2>
+      <label id="label__input" htmlFor="input">
+        Input here!
+      </label>
+      <input type="text" id="input"></input>
+      <a
+        className="article__link"
+        href="https://about.google/?fg=1&utm_source=google-DE&utm_medium=referral&utm_campaign=hp-header"
+      >
+        click here!
+      </a>
+    </article>
+  );
+}
+
+// Your component should contain the following parts:
+
+// - an `<article>` as a wrapper HTML element with the class `article`
+// - an `<h2>` with the class `article__title` and a text of your choice
+// - a `<label>` and `<input>` tag connected with `id` and `htmlFor` (!) attributes
+// - an `<a>` tag with the class `article__link` as well as text content and `href` attribute of your choice (What about a Wikipedia article?)
